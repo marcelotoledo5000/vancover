5ad3dc85c5974c8d9aaa1e79d21b6ffefb42f96e
mateus <mrm220396@yahoo.com>
2019-11-28 19:42:39 -0300

    Squareinch functions added

--- area.go
+++ area.go
@@ -34,3 +34,73 @@ func AcretoSquaremeter(acre float32) float32 {
 func AcretoSquarekilometer(acre float32) float32 {
 	return acre / 247.105
 }
+
+// HectaretoAcre (hectare float32) float32 in Acre value
+func HectaretoAcre(hectare float32) float32 {
+	return hectare * 2.471
+}
+
+// HectaretoSquareinch (hectare float32) float32 in Square Inch value
+func HectaretoSquareinch(hectare float32) float32 {
+	return hectare * 1.55e+7
+}
+
+// HectaretoSquarefoot (hectare float32) float32 in Square Foot value
+func HectaretoSquarefoot(hectare float32) float32 {
+	return hectare * 107639.104
+}
+
+// HectaretoSquareyard (hectare float32) float32 in Square yard value
+func HectaretoSquareyard(hectare float32) float32 {
+	return hectare * 107639.104
+}
+
+// HectaretoSquaremile (hectare float32) float32 in Square mile value
+func HectaretoSquaremile(hectare float32) float32 {
+	return hectare / 258.999
+}
+
+// HectaretoSquaremeter (hectare float32) float32 in Square meter value
+func HectaretoSquaremeter(hectare float32) float32 {
+	return hectare * 10000
+}
+
+// HectaretoSquarekilometer (hectare float32) float32 in Square kilometer value
+func HectaretoSquarekilometer(hectare float32) float32 {
+	return hectare / 100
+}
+
+// SquareinchtoAcre (squareinch float32) float32 in Acre value
+func SquareinchtoAcre(squareinch float32) float32 {
+	return squareinch / 6.273e+6
+}
+
+// SquareinchtoHectare (squareinch float32) float32 in hectare value
+func SquareinchtoHectare(squareinch float32) float32 {
+	return squareinch / 1.55e+7
+}
+
+// SquareinchtoSquarefoot (squarefoot float32) float32 in Square Foot value
+func SquareinchtoSquarefoot(squareinch float32) float32 {
+	return squareinch / 144
+}
+
+// SquareinchtoSquareyard (squareinch float32) float32 in square yard value
+func SquareinchtoSquareyard(squareinch float32) float32 {
+	return squareinch / 1296
+}
+
+// SquareinchtoSquaremile (squareinch float32) float32 Square mile
+func SquareinchtoSquaremile(squareinch float32) float32 {
+	return squareinch / 4.014e+9
+}
+
+// SquareinchtoSquaremeter (squareinch float32) float32 Square meter value
+func SquareinchtoSquaremeter(squareinch float32) float32 {
+	return squareinch / 1550.003
+}
+
+// SquareinchtoSquarekilometer (squareinch float32) float32 Square kilometer value
+func SquareinchtoSquarekilometer(squareinch float32) float32 {
+	return squareinch / 1.55e+9
+}
