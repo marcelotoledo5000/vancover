3ce96a580aab3ac4be65efbcb087f461bd53dbbb
mateus <mrm220396@yahoo.com>
2019-11-28 23:07:11 -0300

    square mile and square yard edded

--- area.go
+++ area.go
@@ -104,3 +104,108 @@ func SquareinchtoSquaremeter(squareinch float32) float32 {
 func SquareinchtoSquarekilometer(squareinch float32) float32 {
 	return squareinch / 1.55e+9
 }
+
+// SquarefoottoAcre (squareinch float32) float32 Square Acre value
+func SquarefoottoAcre(squarefoot float32) float32 {
+	return squarefoot / 43560
+}
+
+// SquarefoottoHectare (squareinch float32) float32 Square hectare value
+func SquarefoottoHectare(squarefoot float32) float32 {
+	return squarefoot / 107639.104
+}
+
+// SquarefoottoSquareinch (squareinch float32) float32 Square Square inch value
+func SquarefoottoSquareinch(squarefoot float32) float32 {
+	return squarefoot * 144
+}
+
+// SquarefoottoSquareyard (squareinch float32) float32 Square Square yard value
+func SquarefoottoSquareyard(squarefoot float32) float32 {
+	return squarefoot / 1296
+}
+
+// SquarefoottoSquaremile (squareinch float32) float32 Square Square mile value
+func SquarefoottoSquaremile(squarefoot float32) float32 {
+	return squarefoot / 4.014e+9
+}
+
+// SquarefoottoSquaremeter (squareinch float32) float32 Square Square meter value
+func SquarefoottoSquaremeter(squarefoot float32) float32 {
+	return squarefoot / 1550.003
+}
+
+// SquarefoottoSquarekilometer (squareinch float32) float32 Square Square kilometer value
+func SquarefoottoSquarekilometer(squarefoot float32) float32 {
+	return squarefoot / 1.55e+9
+}
+
+// SquareyardtoAcre (squareyard float32) float32 acre in value
+func SquareyardtoAcre(squareyard float32) float32 {
+	return squareyard / 4840
+}
+
+// SquareyardtoHectare (squareyard float32) float32 acre in Hectare value
+func SquareyardtoHectare(squareyard float32) float32 {
+	return squareyard / 11959.9
+}
+
+// SquareyardtoSquareinch (squareyard float32) float32 acre in Square inch value
+func SquareyardtoSquareinch(squareyard float32) float32 {
+	return squareyard * 1296
+}
+
+// SquareyardtoSquarefoot (squareyard float32) float32 acre in Square foot value
+func SquareyardtoSquarefoot(squareyard float32) float32 {
+	return squareyard * 9
+}
+
+// SquareyardtoSquaremile (squareyard float32) float32 acre in Square mile value
+func SquareyardtoSquaremile(squareyard float32) float32 {
+	return squareyard / 3.098e+6
+}
+
+// SquareyardtoSquaremeter (squareyard float32) float32 acre in Square meter value
+func SquareyardtoSquaremeter(squareyard float32) float32 {
+	return squareyard / 1.196
+}
+
+// SquareyardtoSquarekilometer (squareyard float32) float32 acre in Square kilometer value
+func SquareyardtoSquarekilometer(squareyard float32) float32 {
+	return squareyard / 1.196e+6
+}
+
+//SquaremiletoAcre (squaremile float32) float32
+func SquaremiletoAcre(squaremile float32) float32 {
+	return squaremile * 640
+}
+
+//SquaremiletoHectare (squaremile float32) float32
+func SquaremiletoHectare(squaremile float32) float32 {
+	return squaremile * 258.999
+}
+
+//SquaremiletoSquareinch (squaremile float32) float32
+func SquaremiletoSquareinch(squaremile float32) float32 {
+	return squaremile * 4.014e+9
+}
+
+//SquaremiletoSquarefoot (squaremile float32) float32
+func SquaremiletoSquarefoot(squaremile float32) float32 {
+	return squaremile * 2.788e+7
+}
+
+// SquaremiletoSquareyard (squaremile float32) float32
+func SquaremiletoSquareyard(squaremile float32) float32 {
+	return squaremile * 3.098e+6
+}
+
+//Squaremiletometer (squaremile float32) float32
+func Squaremiletometer(squaremile float32) float32 {
+	return squaremile * 2.59e+6
+}
+
+//Squaremiletokilometer (squaremile float32) float32
+func Squaremiletokilometer(squaremile float32) float32 {
+	return squaremile * 2.59
+}
